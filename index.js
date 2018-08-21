2a206cf1b892b01a3bb65e42c552c936081a248e
Mark Ehler <ehlerm42@gmail.com>
2018-08-21 23:44:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -71,17 +71,19 @@ removeFromCart('skis');
   
   
 function placeOrder(cardNumber) {
-  if(cardNumber) 
-  {
-    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
-    for (var i = cart.length; i>= 0; i--)
-    { cart.pop();} 
+  if(cardNumber)  {
+      console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    for (var i = getCart().length; i >= 0; i--){ 
+    return cart.pop();
+    } 
   }
   else 
   {
-    console.log('Sorry, we don\'t have a credit card on file for you.')
+    return 'Sorry, we don\'t have a credit card on file for you.'
   }
 }
+
+placeOrder();
 
 
 
