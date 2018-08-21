71712a25614a5b6d6b007344a8b2a47316a8d517
Mark Ehler <ehlerm42@gmail.com>
2018-08-21 23:49:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ removeFromCart('skis');
   
 function placeOrder(cardNumber) {
   if(cardNumber)  {
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
     for (var i = getCart().length; i > 0; i--){ 
     cart.pop();
     i--;
