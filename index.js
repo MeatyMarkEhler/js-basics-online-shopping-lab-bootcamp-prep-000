71f5c55cdbd106f312f009023dc75bcbce8e6ce4
Mark Ehler <ehlerm42@gmail.com>
2018-08-21 23:48:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ removeFromCart('skis');
 function placeOrder(cardNumber) {
   if(cardNumber)  {
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
-    for (var i = getCart().length; i >= 0; i--){ 
+    for (var i = getCart().length; i > 0; i--){ 
     cart.pop();
     i--;
     } 
